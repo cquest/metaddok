fc8d5071598cbe2488dfdf1a002c048b82003fff
cquest <cquest@openstreetmap.fr>
2017-10-22 17:04:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# metaddok
+
+A very simpler multiple addok frontend.
+
+Can be used to query several addok geocoding API managing different address/POI databases with a single request.
+
+The query is sent to each backend, then results are gathered, reorganized, sorted by descending score and finally returned to the client.
---
+++ metaddok.py
@@ -0,0 +1,50 @@
+import falcon
+import json
+import grequests
+
+class GeocodeResource(object):
+    def on_get(self, req, resp):
+        # original request
+        q = req.relative_uri
+
+        # async parallel queries to backends
+        urls = [ 'http://localhost:7878'+q , 'http://localhost:7979'+q, 'http://localhost:7777'+q]
+        rs = (grequests.get(u) for u in urls)
+        results = grequests.map(rs)
+
+        # our global agregated result
+        glob = []
+        for result in results:
+            features = json.loads(result.text)
+            for feature in features['features']:
+                feature['attribution']=features['attribution']
+                glob.append(feature)
+
+        # sort by ascending scores
+        glob = sorted(glob, key=lambda k: k['properties'].get('score', 0), reverse=True)
+
+        # how many results are expected ?
+        if "id" in req.params:
+            limit = int(req.params["id"])
+        else:
+            limit = 5
+        # limit number of results
+        if len(glob)>5:
+            glob = glob[0:5]
+
+        # send back json to client
+        resp.body = json.dumps(dict(type="FeatureCollection", features=glob, licence="ODbL 1.0", version="draft"))
+
+        resp.set_header('X-Powered-By', 'metaddok')
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
+        resp.status = falcon.HTTP_200
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+geocode = GeocodeResource()
+
+# things will handle all requests to the matching URL path
+app.add_route('/search', geocode)
